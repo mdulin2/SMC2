beea300c32591ff6aac8c472eb48ee33d94e8660
l-hartman <ljhartman10@gmail.com>
2019-12-26 22:57:49 -0800

    Added lazy cartesian product implementation.

--- math/dna.py
+++ math/dna.py
@@ -9,9 +9,43 @@
 #----------------------------------------------------------------------
 
 import sys
+import time
 import hashlib
 from itertools import product
 
+# TODO: remove answer
+# solution = "AATGTACACT"
+# solution_hash = hashlib.md5(solution.encode()).hexdigest()
+solution_hash = "6a81c717d2a7cebdaecf766cf85687e4"
+
+def more_efficient_solver():
+    """Python implementation of http://phrogz.net/lazy-cartesian-product"""
+    start = time.time()
+    seq = ["A" for i in range(10)]
+
+    def dive(d):
+        a = "ATCG"
+        if d == 9:
+            for i in range(4):
+                seq[d] = a[i]
+                current_seq = "".join(seq)
+                ans = check_hash(current_seq)
+                if ans:
+                    msg = "Found sequence: %s in %s sec" % (current_seq, str(time.time() - start))
+                    sys.exit(msg)
+        else:
+            for i in range(4):
+                seq[d] = a[i]
+                dive(d+1)                
+        seq[9] = "A"
+    return dive(0)
+
+def check_hash(seq):
+    seq_hash = hashlib.md5(seq.encode()).hexdigest()
+    if seq_hash == solution_hash:
+        return seq
+    else:
+        return ""
 
 def generate_dna_strings(n):
     """
@@ -29,11 +63,6 @@ def generate_dna_strings(n):
 
 def find_matching_hash(sequences):
     """Hashes each sequence till matching hash is found"""
-    # TODO: remove answer
-    # solution = "AATGTACACT"
-    # solution_hash = hashlib.md5(solution.encode()).hexdigest()
-    solution_hash = "6a81c717d2a7cebdaecf766cf85687e4"
-    
     for seq in sequences:
         seq_hash = hashlib.md5(seq.encode()).hexdigest()
         if seq_hash == solution_hash:
@@ -46,6 +75,7 @@ def print_sequences(sequences):
 
 
 def main():
+    """ uncomment to generate all combinations
     try:
         n = int(sys.argv[1])
     except ValueError:
@@ -53,15 +83,14 @@ def main():
     
     # get all possible dna sequences of length n
     possible_sequences = generate_dna_strings(n)
-    # have a look at the beautiful dna sequences you've created, remove if making things slow
-    print_sequences(possible_sequences)
-    
     ans = find_matching_hash(possible_sequences)
     
     if ans:
         print("you found it!!!\n%s" % ans)
     else:
         print("not found... keep looking :)")
+    """
+    more_efficient_solver()
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
